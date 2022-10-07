2eaf77a501db7c0382e9389b05d3ac59d83c3c56
felix-hauger <felix.hauger@laplateforme.io>
2022-10-07 11:39:47 +0200

    added files for job 7 & 9

---
+++ Job7/ftp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+cat sources_template.list > /etc/apt/sources.list
+
+apt update
+apt install proftpd-*
+
+
+
+cat proftpd_template.conf > /etc/proftpd/prodtpd.conf
+cat modules_template.conf > /etc/proftpd/modules.conf
+cat tls_template.conf > /etc/proftpd/tls.conf
+
+
---
+++ Job9/accessrights_ftp.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+#cat -etv <<<"$IFS\n"
+
+oldIFS=$IFS
+
+IFS=","
+
+#cat -etv <<<"$IFS"
+
+while IFS="," read -r user_id user_firstname user_lastname user_password user_role
+do
+
+#	DELETE EXISTING SPACES IN CSV FILE VARIABLES
+
+	user_firstname=${user_firstname//[[:blank:]]/}
+
+	user_lastname=${user_lastname//[[:blank:]]/}
+
+	user_name=$user_firstname$user_lastname
+
+	user_name=${user_name,,}
+
+	user_fullname="$user_firstname $user_lastname"
+
+
+#	CHECK IF USER ID AND GROUP ID EXIST AND ASSIGN THEM TO USER IF THEY DON'T
+
+	id_passwd=$(sudo cat "/etc/passwd" | grep $user_id)
+
+	id_group=$(sudo cat "/etc/group" | grep $user_id)
+
+	if [[ -z $id_passwd && -z $id_group ]]
+	then
+		echo "L'id $user_id et l'id groupe $user_id n'existent pas, assignation des id correspondant"
+		sudo useradd -u $user_id -c "$user_fullname" $user_name
+	else
+		echo "id déjà utilisé, attribution d'id automatique"
+		sudo useradd -c "$user_fullname" $user_name
+	fi
+
+#	REMOVE LAST CHARACTER OF LINE WITH %?
+
+	if [ ${user_role%?} = "Admin" ]
+	then
+		usermod -aG sudo $user_name
+	fi
+
+	usermod -aG ftpgroup $user_name
+
+	echo $user_name:$user_password | chpasswd
+
+done < <(tail -n +2 Shell_Userlist.csv)
+
+
+
+IFS=$oldIFS
+
+#cat -etv <<<"$IFS"
